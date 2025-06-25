fed9a4a07470526a2e16d1adee5bfda755c3655f
ONIKORO <oyamar1989@gmail.com>
2025-06-25 15:58:52 +0900

    del-Ladle

--- stories/welcome.stories.tsx
+++
@@ -1,6 +0,0 @@
-export const Welcome = () => (
-    <div style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
-        <h1>🎉 Ladle導入完了！</h1>
-        <p>このプロジェクトのコンポーネントライブラリへようこそ！</p>
-    </div>
-);
